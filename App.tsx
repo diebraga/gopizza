fe28f0ec6bd0a391ef05a0c9708dbb10d7eca6b2
Diego Braga <diegobraga@hertz.com>
2022-09-09 16:28:57 +0100

    global theme

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
 import { DMSans_400Regular, useFonts } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
 
 export default function App() {
   const [isFontLoaded] = useFonts({
@@ -13,11 +14,11 @@ export default function App() {
   if (!isFontLoaded) {
     return <AppLoading />
   }
-  
-  return (
-    <View>
 
-    </View>
+  return (
+    <ThemeProvider theme={theme}>
+
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+const Test = styled.View`
+  background-color: ${p => p.theme.COLORS.SECONDARY_500};
+`
