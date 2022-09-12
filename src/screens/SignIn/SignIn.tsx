0ad963514fc7b0247231abfde9d08e3e16ee568c
Diego Braga <diegobraga@hertz.com>
2022-09-12 16:13:05 +0100

    added brand

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { Button } from '../../components/Button/Button';
 import { Input } from '../../components/Input/Input';
 
-import { Container } from './styles';
+import BandImg from '../../assets/brand.png'
+
+import { Container, Content, Title, Brand, ForgotPasswordButton, ForgotPasswordLabel } from './styles';
 
 export const SignIn: React.FC = () => {
+
   return (
     <Container>
-      <Input
-        type='secondary'
-        placeholder='email'
-        autoCorrect={false}
-        autoCapitalize='none'
-      />
-      <Input
-        type='secondary'
-        placeholder='password'
-        secureTextEntry
-      />
-      <Button
-        title='Login'
-        isLoading={false}
-        type='secondary'
-      />
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : undefined}>
+        <Content>
+
+          <Brand source={BandImg} />
+          <Title>SignIn</Title>
+          <Input
+            type='secondary'
+            placeholder='email'
+            autoCorrect={false}
+            autoCapitalize='none'
+          />
+          <Input
+            type='secondary'
+            placeholder='password'
+            secureTextEntry
+          />
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>I Forgot my password</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+          <Button
+            title='Login'
+            isLoading={false}
+            type='secondary'
+          />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,5 +1,16 @@
 import { LinearGradient } from "expo-linear-gradient";
 import styled from "styled-components/native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+
+export const Content = styled.ScrollView.attrs(({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBotton: getBottomSpace() + 48
+  }
+}))`
+  width: 100%;
+  padding: 0 32px;
+`
 
 export const Container = styled(LinearGradient).attrs(p => ({
   colors: p.theme.COLORS.GRADIENT,
@@ -8,4 +19,33 @@ export const Container = styled(LinearGradient).attrs(p => ({
 }))`
   justify-content: center;
   flex: 1;
+`
+
+export const Title = styled.Text`
+  font-size: 32px;
+  margin-bottom: 24px;
+  align-self: flex-start;
+
+  font-family: ${p => p.theme.FONTS.TITLE};
+  color: ${p => p.theme.COLORS.TITLE};
+`
+
+export const Brand = styled.Image.attrs({
+  resizeMode: "contain",
+})`
+  height: 340px;
+  margin-top: 64px;
+  margin-bottom: 42px;
+`
+
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  align-self: flex-end;
+  margin-bottom: 20px;
+`
+
+export const ForgotPasswordLabel = styled.Text`
+  font-size: 14px;
+
+  font-family: ${p => p.theme.FONTS.TITLE};
+  color: ${p => p.theme.COLORS.TITLE};
 `
