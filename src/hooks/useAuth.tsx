c2efdef9067096916df26dfc46ddb7c195a4360f
Diego Braga <diegobraga@hertz.com>
2022-09-12 19:32:51 +0100

    get user info

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -6,10 +6,18 @@ import React, {
 } from 'react';
 import { Alert } from "react-native"
 import auth from "@react-native-firebase/auth"
+import firestore from '@react-native-firebase/firestore';
+
+type User = {
+  id: string
+  Name: string
+  isAdmin: boolean
+}
 
 type AuthContexData = {
   signInIsLoading: boolean
   signIn: (email: string, password: string) => Promise<void>
+  user: User | null
 }
 
 type AuthProviderProps = {
@@ -20,6 +28,7 @@ export const AuthContext = createContext({} as AuthContexData)
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [signInIsLoading, setSignInIsLoading] = useState(false)
+  const [user, setUser] = useState<User | null>(null)
 
   const signIn = async (email: string, password: string) => {
     if (!email || !password) {
@@ -30,7 +39,23 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     auth()
       .signInWithEmailAndPassword(email, password)
       .then(account => {
-        console.log(account)
+        firestore()
+          .collection("users")
+          .doc(account.user.uid)
+          .get()
+          .then()
+          .then(profile => {
+            const { Name, isAdmin } = profile.data() as User
+
+            if (profile.exists) {
+              setUser({
+                id: account.user.uid,
+                Name,
+                isAdmin
+              })
+            }
+          })
+          .finally(() => Alert.alert("SignIn", "Sign in has failed"))
       })
       .catch(err => {
         const { code } = err;
@@ -46,7 +71,8 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   return (
     <AuthContext.Provider value={{
       signInIsLoading,
-      signIn
+      signIn,
+      user
     }}>
       {children}
     </AuthContext.Provider>
