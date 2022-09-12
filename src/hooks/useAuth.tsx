edd4db1ac5fc75f64ddf67f8c2065ec9120b0e59
Diego Braga <diegobraga@hertz.com>
2022-09-12 18:36:43 +0100

    login connection with firebase

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -4,9 +4,12 @@ import React, {
   useState,
   ReactNode
 } from 'react';
+import { Alert } from "react-native"
+import auth from "@react-native-firebase/auth"
 
 type AuthContexData = {
-
+  signInIsLoading: boolean
+  signIn: (email: string, password: string) => Promise<void>
 }
 
 type AuthProviderProps = {
@@ -16,9 +19,34 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContexData)
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [signInIsLoading, setSignInIsLoading] = useState(false)
+
+  const signIn = async (email: string, password: string) => {
+    if (!email || !password) {
+      Alert.alert("SignIn", "Email and Password are required fields")
+    }
+
+    setSignInIsLoading(true)
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(account => {
+        console.log(account)
+      })
+      .catch(err => {
+        const { code } = err;
+
+        if (code === "auth/user-not-found" || code === "auth/wrong-password") {
+          return Alert.alert("SignIn", "Invalid email or password")
+        } else {
+          return Alert.alert("SignIn", "Sign in has failed")
+        }
+      })
+      .finally(() => setSignInIsLoading(false))
+  }
   return (
     <AuthContext.Provider value={{
-
+      signInIsLoading,
+      signIn
     }}>
       {children}
     </AuthContext.Provider>
--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, Platform } from 'react-native';
 import { Button } from '../../components/Button/Button';
 import { Input } from '../../components/Input/Input';
@@ -6,8 +6,16 @@ import { Input } from '../../components/Input/Input';
 import BandImg from '../../assets/brand.png'
 
 import { Container, Content, Title, Brand, ForgotPasswordButton, ForgotPasswordLabel } from './styles';
+import { useAuth } from '../../hooks/useAuth';
 
 export const SignIn: React.FC = () => {
+  const { signIn, signInIsLoading } = useAuth()
+  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  
+  const handleOnPress = () => {
+    signIn(email, password)
+  }
 
   return (
     <Container>
@@ -21,19 +29,22 @@ export const SignIn: React.FC = () => {
             placeholder='email'
             autoCorrect={false}
             autoCapitalize='none'
+            onChangeText={setEmail}
           />
           <Input
             type='secondary'
             placeholder='password'
             secureTextEntry
+            onChangeText={setPassword}
           />
           <ForgotPasswordButton>
             <ForgotPasswordLabel>I Forgot my password</ForgotPasswordLabel>
           </ForgotPasswordButton>
           <Button
             title='Login'
-            isLoading={false}
+            isLoading={signInIsLoading}
             type='secondary'
+            onPress={handleOnPress}
           />
         </Content>
       </KeyboardAvoidingView>
