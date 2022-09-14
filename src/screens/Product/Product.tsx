3420ca3f6ca746ba8ead4b9bfff16d946a67646d
Diego Braga <diegobraga@hertz.com>
2022-09-14 12:02:10 +0100

    added image picker

--- src/screens/Product/Product.tsx
+++ src/screens/Product/Product.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
 import ButtonBack from '../../components/ButtonBack/ButtonBack';
 import { Photo } from '../../components/Photo/Photo';
+import * as ImagePicker from 'expo-image-picker';
 
 import { Container, Title, Header, DeleteLabel, PickImageButton, UploadView } from './styles';
 
 const Product: React.FC = () => {
+  const [image, setImage] = useState("")
+
+  const handlePickerPhoto = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status === "granted") {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      const { uri } = result as { uri: string }
+
+      if (!result.cancelled) {
+        setImage(uri);
+      }
+    }
+  }
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Header>
@@ -20,7 +41,11 @@ const Product: React.FC = () => {
       </Header>
       <UploadView>
         <Photo uri='' />
-        <PickImageButton title='Load' type='secondary' />
+        <PickImageButton 
+        title='Load' 
+        type='secondary' 
+        onPress={handlePickerPhoto}
+        />
       </UploadView>
     </Container>
   )
